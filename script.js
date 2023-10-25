85df3c848e91919a17699b984b0e2c24e2a66a0a
Yaseen Nazir <139966479+httpsyaseen@users.noreply.github.com>
2023-10-25 16:13:25 +0500

    finished

---
+++ script.js
@@ -0,0 +1,34 @@
+const state = document.querySelectorAll(".faq-toggle");
+state.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.parentNode.classList.toggle("active");
+  });
+});
+
+const task = document.querySelector(".faq-container");
+const question = document.querySelector(".question-field");
+const answer = document.querySelector(".answer-field");
+
+function added() {
+  const card = document.createElement("div");
+  task.appendChild(card);
+  card.classList.add("faq");
+  //   if (!card.classList.contains("active")) {
+  //     card.classList.add("active");
+  //   }
+  card.innerHTML = ` <h3 class="faq-title">${question.value}</h3>
+
+  <p class="faq-text">${answer.value}</p>
+
+  <button class="faq-toggle" onclick="mychange(this)">
+    <i class="fas fa-chevron-down"></i>
+    <i class="fas fa-times"></i>
+  </button>`;
+
+  question.value = "";
+  answer.value = "";
+}
+
+function mychange(e) {
+  e.parentNode.classList.toggle("active");
+}
